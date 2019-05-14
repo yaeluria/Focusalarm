a3529c0c4277e8e6f7a1fd69ee55c3c0973fa65d
Yael Luria <yaeluria@gmail.com>
2019-05-14 23:18:14 +0300

    add components, logo, background script

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
+import TimeRadioForm from './components/TimeRadioForm'
+import SoundRadioForm from './components/SoundRadioForm'
+
+
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+     
+        <Title/>
+  
+      <TimeRadioForm />
+      <SoundRadioForm />
+
     </div>
-  );
+  )
 }
 
 export default App;
