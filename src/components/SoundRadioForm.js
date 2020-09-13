7f36ee99e16c8085f0fc0b7733afe091a9d5ad64
Yael Luria <yaeluria@gmail.com>
2020-09-13 20:17:07 +0300

    migrate to hooks, use sound files instead of links and play icon instead of audio element

--- src/components/SoundRadioForm.js
+++ src/components/SoundRadioForm.js
@@ -1,116 +1,57 @@
 /*global chrome*/
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import IconButton from '@material-ui/core/IconButton';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 
+export default function SoundRadioForm() {
+  const [value, setValue] = React.useState('Bell');
 
+  React.useEffect(() => {
+    chrome.storage.sync.get(["sound"], (result) => {
+      const sound = result.sound || "Bell";
+      setValue({ value: sound });
+    });
+  }, []);
 
-const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    formControl: {
-        margin: theme.spacing.unit 
+  const selectSound = (event) => {
+    const soundVal = event.target.value;
+    chrome.storage.sync.set({ sound: soundVal }, () => {
+        console.log(`sound is set to ${soundVal}`)
+    });
+    setValue(soundVal);
+  };
+  const playSample = name => {
+    let sampleSound = new Audio(`/${name}.mp3`)
+    sampleSound.play()
+  }
 
-    },
-    group: {
-        margin: `${theme.spacing.unit}px 0`,
-
-    },
-
-    audio: {
-        marginTop: '0.5rem',
-        marginLeft: 'auto'
-    }
-});
-
-
-class SoundRadioForm extends React.Component {
-
-    state = {
-      value: {}
-    };
-
-    handleChange = event => {
-        const soundVal = event.target.value;
-     
-        chrome.storage.sync.set({ 'sound': soundVal }, () => {
-          });
-        this.setState({value: soundVal});
-    };
-
-    componentDidMount() {
-        this.fetchSettings();
-      }  
-      fetchSettings() {
-      
-            chrome.storage.sync.get(['sound'], (result)=>{
-              
-                const sound = result.sound || 'Bell'
-                    this.setState({value: sound});
-            })
-      }  
-   
-    render() {
-        const { classes } = this.props;
- 
-    
-        return (
-            <div className={classes.root}>
-
-                <FormControl component="fieldset" className={classes.formControl}>
-                    <FormLabel component="legend">Sound</FormLabel>
-                    <RadioGroup
-                        row="true"
-                        aria-label="sound"
-                        name="sound"
-                        className={classes.group}
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                    >
-                        <FormControlLabel value="Bell" control={<Radio />} label="Bell" />
-                        <audio
-                            className={classes.audio}
-                            controls
-                            src="https://res.cloudinary.com/drvycak8r/video/upload/v1557737548/storage/30161__herbertboland__belltinystrike.wav">
-                            Your browser does not support the
-                        <code>audio</code> element.
-                        </audio>
-                        <FormControlLabel value="T.rex roar" control={<Radio />} label="T. rex roar" />
-                        <audio
-                            className={classes.audio}
-                            controls
-                            src="https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3">
-                                                Your browser does not support the
-                                <code>audio</code> element.
-                        </audio>
-
-                        <FormControlLabel value="Chirp" control={<Radio />} label="Chirp" />
-                        <audio
-                            className={classes.audio}
-                            controls
-                            src="https://res.cloudinary.com/drvycak8r/video/upload/v1557737433/storage/85403__readeonly__canaryartie-3.wav">
-                            Your browser does not support the
-               <code>audio</code> element.
-           </audio>
-
-
-                    </RadioGroup>
-                </FormControl>
-
-
-            </div>
-        );
-    }
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Sound</FormLabel>
+      <RadioGroup
+        aria-label="sound"
+        name="sound1"
+        value={value}
+        onChange={selectSound}
+      >
+        <FormControlLabel value="Bell" control={<Radio />} label="Bell" />
+        <IconButton onClick={() => playSample("bell")} aria-label="play-sample-bell">
+          <PlayCircleOutlineIcon />
+        </IconButton>
+        <FormControlLabel value="Bleat" control={<Radio />} label="Bleat" />
+        <IconButton onClick={() => playSample("bleat")} aria-label="play-sample-bleat">
+          <PlayCircleOutlineIcon />
+        </IconButton>
+        <FormControlLabel value="Chirp" control={<Radio />} label="Chirp" />
+        <IconButton onClick={() => playSample("chirp")} aria-label="play-sample-chirp">
+          <PlayCircleOutlineIcon />
+        </IconButton>
+      </RadioGroup>
+    </FormControl>
+  );
 }
-
-SoundRadioForm.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SoundRadioForm);
